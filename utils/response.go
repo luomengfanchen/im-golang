7a0c158b61b2a83d95840b0ddb17757817fe1529
Miracle@pro <luomengfanchen@outlook.com>
2021-06-09 19:33:51 +0800

    utils.response添加了注释

--- utils/response.go
+++ utils/response.go
@@ -6,16 +6,22 @@ import (
 	"net/http"
 )
 
+// 封装的josn结构体
 type H map[string]interface{}
 
+// 响应json数据
 func ResponseJSON(w http.ResponseWriter, code int, obj interface{}) {
+	// 设置响应头，返回json数据
 	w.Header().Set("Content-Type","application/json")
+	// 返回状态码
 	w.WriteHeader(code)
 
+	// 解析结构体返回字符串
 	ret, err := json.Marshal(obj)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
+	// 将响应数据返回
 	w.Write(ret)
 }
